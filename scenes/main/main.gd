9f4eb8e511919c07a4242a325785fd04fa4714bd
Butkeim <yanis.kerrad@gmail.com>
2022-10-01 12:21:06 +0200

    exit on Escape, fullscreen on F11

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,16 +1,19 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#OS.current_screen = 1
+	OS.window_fullscreen = true
+	#OS.window_borderless = true
+	
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+
+	if Input.is_action_just_pressed("full_screen"):
+		OS.window_fullscreen = !OS.window_fullscreen
+
+	if Input.is_action_just_released("ui_cancel"):
+		get_tree().quit()
+
