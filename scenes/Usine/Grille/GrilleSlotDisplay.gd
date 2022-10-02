62c578aaccade7b9cc99cda514554ddb989a239d
MrSchlag <oscar.lambrun@club-internet.fr>
2022-10-02 17:14:33 +0200

    work around pos item drag and drop

--- scenes/Usine/Grille/GrilleSlotDisplay.gd
+++ scenes/Usine/Grille/GrilleSlotDisplay.gd
@@ -3,6 +3,12 @@ extends CenterContainer
 var grille = preload("res://scenes/Usine/Grille/Grille.tres")
 onready var ObjectTextureRect = $ObjectTextureRect
 
+var dragged: TextureRect = null
+
+func _process(delta: float) -> void:
+	if is_instance_valid(dragged):
+		dragged.set_position(get_global_mouse_position())
+
 func display_item(item):
 	if item is objects:
 		ObjectTextureRect.texture  = item.texture
@@ -11,7 +17,6 @@ func display_item(item):
 	else:
 		ObjectTextureRect.texture = load("res://assets/sprites/factory/factory_tile/grid.png")
 		#ObjectTextureRect.rect_scale = Vector2(1,1)
-		
 		
 func get_drag_data(_position):
 	var item_index = get_index()
@@ -22,6 +27,7 @@ func get_drag_data(_position):
 		data.object_index = item_index
 		var dragPreview = TextureRect.new()
 		dragPreview.texture = item.texture
+		dragged = dragPreview
 		set_drag_preview(dragPreview)
 		return	data
 	
@@ -37,4 +43,5 @@ func drop_data(_position, data):
 	var my_item = grille.objects[my_item_index]
 	grille.swap_items(my_item_index, data.object_index)
 	grille.set_item(my_item_index, data.object)
+	dragged = null
 	pass
